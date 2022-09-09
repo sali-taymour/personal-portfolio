f89df7c4f5067ab78bede3c5b64f30414e40f003
sali <sali.taymor@gmail.com>
2022-09-10 00:21:25 +0200

    added logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../assets/logo-portfolio0.png";
+import {faBars, faTimes} from 'react-icons/fa' 
+export default function Navbar() {
+    return (
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#2F4858]">
+            <div>
+                <img src={Logo} alt="Logo" style= {{width: '83px'}} />
+            </div>
+        </div>
+    );
+}
