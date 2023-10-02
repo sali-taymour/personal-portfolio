2e3a74ab084df56ba7d08e1f5fe43e674e22b4c4
Sali.Taymour <sali.taymour@gmail.com>
2023-10-03 01:49:34 +0200

    add skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,6 +9,7 @@ import GIT from "../assets/Git.png";
 import GitHub from "../assets/github.png";
  import Tailwind from "../assets/tailwind.png";
 import Mongo from "../assets/mongo.png";
+import cypress from "../assets/cypress.1024x1022.png";
 
 const Skills = () => {
     return (
@@ -91,6 +92,24 @@ const Skills = () => {
                             alt="HTML icon"
                         />
                         <p className="my-4">GIT</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img
+                            className="w-20 mx-auto"
+                            src={GIT}
+                            alt="HTML icon"
+                        />
+                        <p className="my-4">End 2 End Testing</p>n
+
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img
+                            className="w-20 mx-auto"
+                            src={cypress}
+                            alt="HTML icon"
+                        />
+                        <p className="my-4">Cypress</p>n
+
                     </div>
                     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
                         <img
